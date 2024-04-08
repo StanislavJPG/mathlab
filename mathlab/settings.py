ecaf3a76b93750279aea5597f86ec1df95da4baa
StanislavJPG <samper.stas@gmail.com>
2024-04-09 01:02:06 +0300

    update setting.py

--- mathlab/settings.py
+++ mathlab/settings.py
@@ -175,5 +175,5 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'samper.stas@gmail.com'
-EMAIL_HOST_PASSWORD = 'xldwtqlojywosgmd'
+EMAIL_HOST_USER = os.getenv('MAIL_NAME')
+EMAIL_HOST_PASSWORD = os.getenv('MAIL_PASS')

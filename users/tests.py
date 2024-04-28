95218e2524a93d44ce6a5e957d847819124cc427
StanislavJPG <samper.stas@gmail.com>
2024-04-28 19:54:29 +0300

    unittests for forum/user added

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,44 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.authtoken.models import Token
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+from users.models import CustomUser
+
+
+class TestAccount(APITestCase):
+    def setUp(self):
+        # registration test
+
+        url = reverse('register_view')
+        data = {'email': 'asdas12@asd',
+                'username': 'username',
+                'password': 'passwordPASSWORD'}
+
+        response = self.client.post(url, data)
+
+        self.assertEqual(response.status_code, status.HTTP_302_FOUND)
+
+    def test_auth_user(self):
+        url = reverse('login_view')
+        data = {'email': 'asdas12@asd',
+                'password': 'passwordPASSWORD'}
+
+        response = self.client.post(url, data)
+
+        token = Token.objects.get(user__username='username')
+        self.client.credentials(HTTP_AUTHORIZATION=f'Token {token}')
+
+        self.assertEqual(response.status_code, status.HTTP_302_FOUND)
+
+    def test_logout_user(self):
+        url = reverse('logout_view')
+        response = self.client.post(url)
+
+        self.assertEqual(response.status_code, status.HTTP_302_FOUND)
+
+    def test_password_reset(self):
+        url = reverse('password_reset')
+        response = self.client.post(url, data={'email': 'asdas12@asd'})
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)

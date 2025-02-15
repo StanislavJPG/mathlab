9109cce7d6121def1a17b24a3a69e7033926f0ae
StanislavJPG <samper.stas@gmail.com>
2025-02-15 22:42:59 +0200

    chore(forum): Fix some details and start working with profile

--- server/common/templatetags/pagination.py
+++
@@ -1,8 +0,0 @@
-from django import template
-
-register = template.Library()
-
-
-@register.filter(name="page_convert")
-def page_converter(stop, step):
-    return [i // step + 1 for i in range(0, stop, step)]

2809532500035623752cd10b93d60aa634ebcd3b
StanislavJPG <samper.stas@gmail.com>
2025-03-13 00:25:57 +0200

    chore(captcha): Add describe to js

--- server/static/js/refresh_captcha.js
+++ server/static/js/refresh_captcha.js
@@ -3,4 +3,4 @@ $('#refresh-captcha').click(function () {
         $('.captcha').attr('src', result['image_url']);
         $('#id_captcha_0').val(result['key']);
     });
-});
+}); // see: https://django-simple-captcha.readthedocs.io/en/latest/usage.html

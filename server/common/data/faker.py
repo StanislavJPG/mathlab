2ebdd4a4eddbf9e9cd15be7297a55fb8c0662b28
StanislavJPG <samper.stas@gmail.com>
2025-05-23 00:49:24 +0300

    chore(data): Upgrade faker data generator classes

--- server/common/data/faker.py
+++ server/common/data/faker.py
@@ -3,37 +3,37 @@ from server.apps.theorist.factories import TheoristFactory
 from server.apps.users.factories import CustomUserFactory
 
 
-class FakeDataGenerator(object):
+class BaseFakeDataGenerator(object):
     default_success_label = '%s %s was successfully generated.'
 
     @classmethod
-    def generate_fake_users(cls, repeats: int = 1):
+    def _base_generator(cls, *, factory_class, iterations: int = 1, label='fake instances'):
         print('Generation of fake data...')
-        for _ in range(repeats):
-            CustomUserFactory.create()
+        for _ in range(iterations):
+            factory_class.create()
         print('Generation of fake data is done!')
-        return cls.default_success_label % (repeats, 'fake users')
+        return cls.default_success_label % (iterations, label)
+
+
+class FakeDataGenerator(BaseFakeDataGenerator):
+    """
+    Basically, it's possible to generate fake data with own factory using `_base_generator()`
+    But child class `FakeDataGenerator` provides convenient way to create fake data via
+    already declared methods
+    """
 
     @classmethod
-    def generate_fake_theorists(cls, repeats: int = 1):
-        print('Generation of fake data...')
-        for _ in range(repeats):
-            TheoristFactory.create()
-        print('Generation of fake data is done!')
-        return cls.default_success_label % (repeats, 'fake theorists')
+    def generate_fake_users(cls, iterations: int = 1):
+        return cls._base_generator(factory_class=CustomUserFactory, iterations=iterations, label='fake users')
 
     @classmethod
-    def generate_fake_posts(cls, repeats: int = 1):
-        print('Generation of fake data...')
-        for _ in range(repeats):
-            PostFactory.create()
-        print('Generation of fake data is done!')
-        return cls.default_success_label % (repeats, 'fake posts')
+    def generate_fake_theorists(cls, iterations: int = 1):
+        return cls._base_generator(factory_class=TheoristFactory, iterations=iterations, label='fake theorists')
 
     @classmethod
-    def generate_fake_comments(cls, repeats: int = 1):
-        print('Generation of fake data...')
-        for _ in range(repeats):
-            CommentFactory.create()
-        print('Generation of fake data is done!')
-        return cls.default_success_label % (repeats, 'fake comments')
+    def generate_fake_posts(cls, iterations: int = 1):
+        return cls._base_generator(factory_class=PostFactory, iterations=iterations, label='fake posts')
+
+    @classmethod
+    def generate_fake_comments(cls, iterations: int = 1):
+        return cls._base_generator(factory_class=CommentFactory, iterations=iterations, label='fake comments')

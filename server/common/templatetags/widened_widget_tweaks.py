1c9f232fa45d6fa75cb43c441244a5ed09559428
StanislavJPG <samper.stas@gmail.com>
2025-05-29 18:38:37 +0300

    feat(project): Start to add spinners to UI buttons

--- server/common/templatetags/widened_widget_tweaks.py
+++ server/common/templatetags/widened_widget_tweaks.py
@@ -26,3 +26,27 @@ def render_field_errors(field, class_='text-danger mb-2', small=False):
         </div>
     """
     return format_html(text_to_render)
+
+
+@register.simple_tag(name='render_form_spinner_attrs')
+def render_form_attrs_with_spinner(target_spinner_id='wait-spinner'):
+    attrs_to_render = f'_="on submit toggle @hidden on #{target_spinner_id} until htmx:afterOnLoad"'
+    return format_html(attrs_to_render)
+
+
+@register.simple_tag(name='render_submit_button')
+def render_bootstrap_spinner_submit_button(
+    label='',
+    *,
+    spinner_id='wait-spinner',
+    spinner_class='spinner-grow text-light spinner-grow-sm',
+    with_icon=None,
+    **kwargs,
+):
+    attrs = ' '.join([f'{k}="{v}"' for k, v in kwargs.items()])
+    spinner = f'<div id="{spinner_id}" class="{spinner_class}" role="status" hidden></div>'
+    icon = f'<i class="{with_icon}"></i>' if with_icon else ''
+    class_ = kwargs.get('class', 'btn btn-primary')
+
+    btn_to_render = f'<button type="submit" class="{class_}" {attrs}>{spinner} {icon} {label}</button>'
+    return format_html(btn_to_render)

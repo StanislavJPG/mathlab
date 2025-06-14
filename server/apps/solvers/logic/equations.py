9682d89984c16e2d50a6a14106655cd2c579c307
StanislavJPG <samper.stas@gmail.com>
2025-06-14 12:10:13 +0300

    fix(solver): Fix some details and add i18n

--- server/apps/solvers/logic/equations.py
+++ server/apps/solvers/logic/equations.py
@@ -2,4 +2,6 @@ from django.views.generic import TemplateView
 
 
 class EquationsTemplateView(TemplateView):
+    """Maybe in the future, will make this functionality only for auth users or only for paid."""
+
     template_name = 'partials/equations.html'

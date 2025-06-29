1e20b95d4dd1aa05a433e93e307bcb4298c1952f
StanislavJPG <samper.stas@gmail.com>
2025-06-29 16:26:47 +0300

    feat(community): Add properly filter and other changes and fixes

--- server/apps/theorist_community/filters.py
+++ server/apps/theorist_community/filters.py
@@ -1,5 +1,6 @@
 import django_filters as filters
 
+from server.apps.theorist.choices import TheoristRankChoices
 from server.apps.theorist.models import Theorist
 
 
@@ -7,7 +8,14 @@ class TheoristCommunityFilter(filters.FilterSet):
     full_name = filters.CharFilter(
         lookup_expr='icontains',
     )
+    rank = filters.ChoiceFilter(
+        choices=TheoristRankChoices.choices,
+        field_name='rank',
+    )
 
     class Meta:
         model = Theorist
-        fields = ('full_name',)
+        fields = (
+            'full_name',
+            'rank',
+        )

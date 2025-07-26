b667041811e2b3b5a6862ec8b6e2f65d51e36746
StanislavJPG <samper.stas@gmail.com>
2025-07-26 14:10:07 +0300

    feat(graphbuilder): Fully fix graphbuilder logic

--- server/apps/solvers/logic/solvers.py
+++ server/apps/solvers/logic/solvers.py
@@ -1,9 +1,12 @@
 import matplotlib
 import mpld3
 import numpy as np
+from django.utils.html import format_html
+from render_block import render_block_to_string
+from latex2sympy2 import latex2sympy
+from sympy import lambdify, symbols
 from braces.views import FormInvalidMessageMixin
 from django.http import HttpResponse
-from django.template.loader import render_to_string
 from django.views.generic import TemplateView
 from django.utils.translation import gettext_lazy as _
 from matplotlib import pyplot as plt
@@ -29,30 +32,44 @@ class GraphBuilderTemplateView(FormInvalidMessageMixin, TemplateView):
         if form.is_valid():
             fig = plt.figure()
 
-            x_values = np.linspace(-10, 10, 100)
-            y_values = eval(function, {'x': x_values, 'np': np})
+            x = symbols('x')
+            sym_expr = latex2sympy(function)
 
-            plt.plot(x_values, y_values)
+            f = lambdify(x, sym_expr, modules=['numpy'])
+
+            x_vals = np.linspace(-10, 10, 100)
+            y_vals = f(x_vals)
+            if np.isscalar(y_vals):
+                y_vals = np.full_like(x_vals, y_vals)
+
+            plt.plot(x_vals, y_vals, label=function)
             plt.xlabel('x')
             plt.ylabel('y')
 
             html_graph = mpld3.fig_to_html(fig)
 
-            context = {**self.get_context_data(), 'form': form, 'graph': [html_graph]}
-            block_form = render_to_string(self.template_name, context, request=self.request)
+            context = {'graph': html_graph}
+            block_form = render_block_to_string(self.template_name, 'graph_block', context, request=self.request)
             return HttpResponse(content=block_form)
-        else:
-            self.messages.error(self.request, self.get_form_invalid_message())
-            return HttpResponse()
+
+        return self._render_error()
+
+    def _render_error(self):
+        message = self.get_form_invalid_message()
+        self.messages.error(message, fail_silently=True)
+        html_error_msg = format_html(
+            f"""
+            <div class="alert alert-danger mt-4" role="alert">
+              {message}
+            </div>
+            """
+        )
+        block_form = render_block_to_string(
+            self.template_name, 'graph_block', {'graph': html_error_msg}, request=self.request
+        )
+        return HttpResponse(content=block_form)
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['form'] = GraphBuilderForm()
         return context
-
-    # def get_context_data(self, **kwargs):
-    #     context = super().get_context_data(**kwargs)
-    #     function = self.request.GET.get('function')
-    #     if function:
-    #
-    #     return context

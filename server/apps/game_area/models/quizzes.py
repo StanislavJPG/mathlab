4297ffa4ecea31db5844bf707312283e4e4eded1
StanislavJPG <samper.stas@gmail.com>
2025-08-21 01:09:25 +0300

    chore(quizzes): Add some models fields and HTML improvements

--- server/apps/game_area/models/quizzes.py
+++ server/apps/game_area/models/quizzes.py
@@ -1,5 +1,6 @@
+from django.core.exceptions import ValidationError
 from django.db import models
-from django.db.models import Avg, Sum
+from django.db.models import Avg, Sum, Count
 from django.utils.translation import gettext_lazy as _
 from django_lifecycle import LifecycleModel, hook, AFTER_CREATE, AFTER_SAVE
 from dynamic_filenames import FilePattern
@@ -26,7 +27,7 @@ class MathExpression(UUIDModelMixin, TimeStampedModelMixin, LifecycleModel):
     score_reward = models.PositiveSmallIntegerField(verbose_name=_('score reward for the single expression'), default=3)
 
     class Meta:
-        ordering = ('-created_at',)
+        ordering = ('created_at',)
         verbose_name = _('Math Expression')
         verbose_name_plural = _('Math Expressions')
 
@@ -63,67 +64,96 @@ class MathQuiz(UUIDModelMixin, TimeStampedModelMixin, LifecycleModel):
         verbose_name=_('score reward for quiz finishing'), default=15
     )
 
+    author = models.ForeignKey('theorist.Theorist', on_delete=models.SET_NULL, null=True, blank=True)
+    author_name_slug = models.SlugField(max_length=255, blank=True)  # to save history while theorist being deleted
+
     average_solve_time_statistic = models.DurationField(verbose_name=_('average solve time'), null=True, blank=True)
 
     max_time_to_solve = models.DurationField(verbose_name=_('max time to solve'))
     math_expressions_count = models.PositiveSmallIntegerField(default=0, blank=True)  # denormilized field
 
+    # objects = ...
+
     class Meta:
-        ordering = ('-created_at',)
+        ordering = ('created_at',)
         verbose_name = _('Math Quiz')
         verbose_name_plural = _('Math Quizzes')
 
     def __str__(self):
         return f'Quiz | {self.get_category_display()} | {self.__class__.__name__} | id - {self.id}'
 
+    @hook(AFTER_CREATE)
+    def after_create(self):
+        self.author_name_slug = slugify(self.author.full_name)
+        self.save(update_fields=['author_name_slug'])
+
 
 class MathSolvedQuizzes(TimeStampedModelMixin, UUIDModelMixin, LifecycleModel):
     math_quiz = models.ForeignKey('game_area.MathQuiz', on_delete=models.CASCADE)
     math_quiz_scoreboard = models.ForeignKey('game_area.MathQuizScoreboard', on_delete=models.CASCADE)
-    best_time_taken = models.DurationField(verbose_name=_('best time taken'), blank=True)
+    best_time_taken = models.DurationField(verbose_name=_('best time taken to finish this quiz'), blank=True)
 
     class Meta:
         verbose_name = _('Math solved quizzes')
         unique_together = (('math_quiz', 'math_quiz_scoreboard'),)
 
+    def clean(self):
+        if self.best_time_taken > self.math_quiz.max_time_to_solve:
+            raise ValidationError(
+                _('You have only %s to successfully finish this quiz.') % self.math_quiz.max_time_to_solve
+            )
+
     @hook(AFTER_SAVE)
     def after_save(self):
+        self.collect_solved_quiz_statistics()
+
+    def collect_solved_quiz_statistics(self):
         def average_pass_time(expr):
             return self.__class__.objects.filter(**expr).aggregate(avg_pass_time=Avg('best_time_taken'))[
                 'avg_pass_time'
             ]
 
-        if (
+        is_save_best_time = (
             average_pass_time({'math_quiz_scoreboard': self.math_quiz_scoreboard})
-            < self.math_quiz_scoreboard.time_taken
-        ):
-            # save only best time
+            < self.math_quiz_scoreboard.time_taken_to_solve_quizzes
+            if self.math_quiz_scoreboard.time_taken_to_solve_quizzes
+            else True
+        )  # save only best time of save anyway if there is time_taken_to_solve_quizzes is None
+        if is_save_best_time:
             self.math_quiz.average_solve_time_statistic = average_pass_time({'math_quiz': self.math_quiz})
-            self.math_quiz_scoreboard.time_taken = average_pass_time(
+            self.math_quiz_scoreboard.time_taken_to_solve_quizzes = average_pass_time(
                 {'math_quiz_scoreboard': self.math_quiz_scoreboard}
             )
 
             self.math_quiz.save(update_fields=['average_solve_time_statistic'], skip_hooks=True)
-            self.math_quiz_scoreboard.save(update_fields=['time_taken'], skip_hooks=True)
+            self.math_quiz_scoreboard.save(update_fields=['time_taken_to_solve_quizzes'], skip_hooks=True)
+
+        most_popular_category = (
+            self.math_quiz_scoreboard.solved_quizzes.values_list('category', flat=True)
+            .annotate(most_popular_categories=Count('category'))
+            .order_by('-most_popular_categories')[0]
+        )
+        self.math_quiz_scoreboard.most_popular_quiz_type = most_popular_category
+        self.math_quiz_scoreboard.save(update_fields=['most_popular_quiz_type'], skip_hooks=True)
 
 
 class MathQuizScoreboard(UUIDModelMixin, TimeStampedModelMixin, LifecycleModel):
     solved_expressions = models.ManyToManyField('game_area.MathExpression', blank=True)
     solved_quizzes = models.ManyToManyField('game_area.MathQuiz', through='game_area.MathSolvedQuizzes', blank=True)
 
-    solved_by = models.ForeignKey('theorist.Theorist', on_delete=models.SET_NULL, null=True)
+    solved_by = models.ForeignKey('theorist.Theorist', on_delete=models.SET_NULL, null=True, blank=True)
     solved_by_name_slug = models.SlugField(max_length=255, blank=True)  # to save history while theorist being deleted
 
     most_popular_quiz_type = models.CharField(max_length=2, choices=MathQuizCategoryChoices)
-    time_taken = models.DurationField(
+    time_taken_to_solve_quizzes = models.DurationField(
         verbose_name=_('time that was taken to solve all quizzes'), blank=True, null=True
-    )  # change semantic
+    )
     board_score = models.CharField(
         choices=MathQuizScoreboardScoreChoices, default=MathQuizScoreboardScoreChoices.EMPTY, max_length=2
     )
 
     class Meta:
-        ordering = ('-created_at',)
+        ordering = ('created_at',)
         verbose_name = _('Math Scoreboard')
         verbose_name_plural = _('Math Scoreboards')
 
